775d782ddb98d878bdb352546e3d324e8ee794e0
Fonta97 <fontana.alessandro97@gmail.com>
2024-05-19 22:38:36 +0200

    sucuni

---
+++ la-mia-pizzeria-razor-layout/Models/Pizza.cs
@@ -0,0 +1,19 @@
+﻿namespace la_mia_pizzeria_razor_layout.Models
+{
+    public class Pizza
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public string img { get; set; }
+        public decimal price { get; set; }
+
+        public Pizza(string name, string description, string img, decimal price)
+        {
+            this.name = name;
+            this.description = description;
+            this.img = img;
+            this.price = price;
+        }
+    }
+}
+
